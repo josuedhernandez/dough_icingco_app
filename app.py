506dc09db0fac1e069e47e34f77075b58076dc10
nate smith <python.loop@gmail.com>
2026-01-23 14:11:10 -0800

    added about page, links, and minor styling

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
   return render_template( "index.html", header = "Dough & Icing Co.", content = "Welcome to the bakery!")
+
+@app.route("/about")
+def about():
+  return render_template( "about.html", header = "About Us", content = "We are a family-owned bakery specializing in delicious custom sugar cookies and beautiful sourghdough bread.")
 
 if __name__ == "__main__":
   app.run(debug=True)
